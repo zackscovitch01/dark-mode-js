d8fc7498ad5573f2ea12395df06e63b25dfc3a90
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-05 21:06:34 -0400

    toggleBtn

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('dark mode');
+console.log("dark mode");
+
+const toggleBtn = document.querySelector(".btn");
+const articles = [...document.querySelectorAll(".post")];
+toggleBtn.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark-theme");
+});
+
+articles.forEach((article) => {
+  console.log(article);
+});
